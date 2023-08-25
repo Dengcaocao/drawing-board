1d6a5c6d84761241fc62d0593f86065f0754e4d3
dw <1536698087@qq.com>
2023-08-25 17:01:14 +0800

    注册pinia

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import './assets/base.css'
+
+const pinia = createPinia()
 const app = createApp(App)
 
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
